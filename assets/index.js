613f9d2040cdfc8f6c85faf31775c28762575f75
Blake Edwards <blakeedwards.nc@gmail.com>
2023-03-28 13:11:22 -0400

    Added a const with questions to index.js

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your product?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter the usage information:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please choose a lincense:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter the contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please enter test instructions:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
